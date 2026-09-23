5585aeb71d851225ff074cf73815ee82ef266aad
kel <kelmank@protonmail.com>
2026-09-22 19:43:26 -0500

    Disable sandbox so preload bridge loads

--- src/main/index.ts
+++ src/main/index.ts
@@ -323,6 +323,7 @@ function createWindow() {
       preload: join(__dirname, '../preload/index.mjs'),
       contextIsolation: true,
       nodeIntegration: false,
+      sandbox: false,
     },
   })
 
